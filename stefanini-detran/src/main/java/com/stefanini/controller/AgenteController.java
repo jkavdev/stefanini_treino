6df51bfef70f12990d1f11a748672eb3cbabdab4
jkavdev <jhonatankolen@gmail.com>
2016-11-10 10:33:04 -0200

    Retornando agentes do banco

--- stefanini-detran/src/main/java/com/stefanini/controller/AgenteController.java
+++ stefanini-detran/src/main/java/com/stefanini/controller/AgenteController.java
@@ -1,44 +1,28 @@
 package com.stefanini.controller;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.stefanini.model.Agente;
+import com.stefanini.service.AgenteService;
 
 @Path("/agente")
 @RequestScoped
 public class AgenteController {
+	
+	@Inject
+	private AgenteService agenteService;
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Agente> get() {
-
-		List<Agente> listaAgente = new ArrayList<Agente>();
-		Agente agente1 = new Agente();
-		Agente agente2 = new Agente();
-
-		agente1.setId(1L);
-		agente1.setNome("Diogo");
-		agente1.setDataContratacao(new Date(2016 - 04 - 05));
-		agente1.setTempoServico(11);
-
-		listaAgente.add(agente1);
-
-		agente2.setId(2L);
-		agente2.setNome("Zezinho");
-		agente2.setDataContratacao(new Date(2016 - 04 - 15));
-		agente2.setTempoServico(1);
-
-		listaAgente.add(agente2);
-
-		return listaAgente;
+		return agenteService.todos();
 	}
 
 }
